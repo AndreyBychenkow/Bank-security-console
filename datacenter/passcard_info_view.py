71244f94d9fea015ccd3217d59cb40b996c5145d
AndreyBychenkow <decebell032@yandex.ru>
2024-10-04 19:24:59 +0300

    linked numbers to code

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -4,17 +4,21 @@ from django.utils.timezone import localtime
 from datacenter.models import Passcard
 from datacenter.models import Visit
 
+HOURS_IN_SECONDS = 3600
+MINUTES_IN_SECONDS = 60
+LONG_VISIT_THRESHOLD = 60
+
 
 def format_duration(duration):
     total_seconds = int(duration.total_seconds())
-    hours, remainder = divmod(total_seconds, 3600)
-    minutes, seconds = divmod(remainder, 60)
+    hours, remainder = divmod(total_seconds, HOURS_IN_SECONDS)
+    minutes, seconds = divmod(remainder, MINUTES_IN_SECONDS)
     return f'{hours:02}:{minutes:02}:{seconds:02}'
 
 
-def is_visit_long(visit, minutes=60):
+def is_visit_long(visit):
     duration = visit.leaved_at - visit.entered_at
-    return duration.total_seconds() // 60 > minutes
+    return duration.total_seconds() // MINUTES_IN_SECONDS > LONG_VISIT_THRESHOLD
 
 
 def passcard_info_view(request, passcode):
